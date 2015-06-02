40e36e8144765549fad9843569c8787b530e236b
Taylor McClure <Taylor@Dev-Machine.home>
2015-06-02 17:16:05 -0600

    Added Welcome page, changed some UI components

--- www/js/app.js
+++ www/js/app.js
@@ -41,6 +41,15 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   })
 
+  .state('app.welcome', {
+    url: "/welcome",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/welcome.html"
+      }
+    }
+  })
+
   .state('app.days', {
     url: "/days",
     views: {
@@ -121,5 +130,5 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/home');
+  $urlRouterProvider.otherwise('/app/welcome');
 });
