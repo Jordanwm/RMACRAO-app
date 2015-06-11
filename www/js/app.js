895e9144419e3657e5d5d96f823219df330b7323
Jordanwm <jmoser@mymail.mines.edu>
2015-06-11 17:24:30 -0600

    Added loading for speakers

--- www/js/app.js
+++ www/js/app.js
@@ -6,7 +6,7 @@
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ionic.utils'])
 
-.run(function($ionicPlatform, $http, $location, Year, Exhibitors) {
+.run(function($ionicPlatform, $http, $location, Year, Exhibitors, Speakers) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -20,6 +20,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
 
     Year.loadFromWeb();
     Exhibitors.loadFromWeb();
+    Speakers.loadFromWeb();
 
     //$location.path("/welcome");
   });
